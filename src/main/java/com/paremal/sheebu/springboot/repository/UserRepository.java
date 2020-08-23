483c2e150ac64da97bc6799da0529c2eedd1f51d
Sheebu <Sheebu@Paremal>
2020-08-23 21:37:17 +0530

    db connection with data jpa added

---
+++ src/main/java/com/paremal/sheebu/springboot/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.paremal.sheebu.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.paremal.sheebu.springboot.entity.User;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+
+}
